87729be81ab1421ec835a217f8415eca82de4d94
bflanagin <bflanagin@gmail.com>
2022-05-04 14:24:03 -0600

    added backlight switching

--- battery/benchmark.py
+++ battery/benchmark.py
@@ -44,17 +44,23 @@ def power_profile():
     if os.path.exists("/usr/bin/system76-power"):
         #print("found system76-power")
         info["graphics"] = subprocess.run(["system76-power","graphics"],capture_output=True,text=True).stdout.split("\n")[0]
-        
-    bmax = sys_file("/sys/class/backlight/acpi_video0/","max_brightness")
-    bcurrent = sys_file("/sys/class/backlight/acpi_video0/","brightness")
-    info["backlight"] = {
-                         "max":float(bmax),
-                         "current":float(bcurrent),
-                         "precent":float(bcurrent)/float(bmax)
-                        }
+    
+   # bmax = sys_file("/sys/class/backlight/acpi_video0/","max_brightness")
+  #  bcurrent = sys_file("/sys/class/backlight/acpi_video0/","brightness")
+   # info["backlight"] = {
+   #                      "max":float(bmax),
+    #                     "current":float(bcurrent),
+    #                     "precent":float(bcurrent)/float(bmax)
+     #                   }
         
     return info
-    
+
+def switch_power_profile(mode):
+    if os.path.exists("/usr/bin/system76-power"):
+        output = subprocess.run(["system76-power","profile",mode],capture_output=True,
+                text=True
+                ).stdout.split("\n")[0]
+        return output
 
 def gather_info(infoType):
     print("\n")
@@ -77,10 +83,66 @@ def run_app(app):
                 app
                 )
     return output
+    
+def set_backlight(level):
+    root = "/sys/class/backlight/"
+    blmax = 100
+    blmin = 1
+    if os.path.exists(root+"intel_backlight"):
+        root = root+"intel_backlight/"
+    if os.path.exists(root+"amdgpu_bl0"):
+        root = root+"amdgpu_bl0/"
+        
+    blmax = sys_file(root,"max_brightness")
+    set_level = level * float(blmax)
+    output = subprocess.run(
+               ["echo "+str(int(set_level))+" | sudo tee "+root+"brightness"],
+                capture_output=True,
+                shell=True,
+               text=True
+               ).stdout.split("\n")[0]
+        
+    
+    
+def run_test(name,duration,apps,backlight):
 
+    begin = {}
+    end = {}
+    #os.system("clear") 
+    print("Running test",name)
+    print("  Backlight =", backlight)
+    print("  Durtation =", duration,"minutes")
+    set_backlight(backlight)
+    for a in apps:
+        print("  Launching",a)
+        run_app(a)
+    
+    gather_info("all")
+    begin = battery()
+    time.sleep(duration*60)
+    end = battery()
+    gather_info("all")
+    battery_loss = float(begin["charge_now"]) - float(end["charge_now"])
+    battery_life = float(duration) * (float(begin["charge_full"]) / battery_loss) 
+    print("\nTest complete")
+    print("Percent battery loss in",duration,"minutes is:",float(begin["percent"]) - float(end["percent"]),"%")
+    print("estimated battery life:",battery_life,"minutes -or-",battery_life/60)
 
 if args.quick:
     print("Running quick battery tests")
+    gather_info("all")
+    print("\n")
+    switch_power_profile("battery")
+    run_test("battery profile",5,[],0.1)
+    time.sleep(10)
+    print("\n")
+    switch_power_profile("balanced")
+    run_test("balanced profile",5,[],0.1)
+    time.sleep(10)
+    print("\n")
+    switch_power_profile("performance")
+    run_test("performance profile",5,[],0.1)
+    
     
 if args.info:
     gather_info("all")
@@ -96,9 +158,7 @@ if args.test:
     duration = 10
     apps = []
     backlight = 0.5
-    begin = {}
-    end = {}
-    
+
     
     if os.path.exists(args.test):
         test = open(args.test,"r")
@@ -114,23 +174,5 @@ if args.test:
             name = test_settings["name"]
         if "apps" in test_settings.keys():
             apps = test_settings["apps"]
-            
-    os.system("clear")    
-    print("Running test",name)
-    print("  Backlight =", backlight)
-    print("  Durtation =", duration,"minutes")
-    for a in apps:
-        print("  Launching",a)
-        run_app(a)
-    
-    gather_info("all")
-    begin = battery()
-    time.sleep(duration*60)
-    end = battery()
-    gather_info("all")
-    battery_loss = float(begin["charge_now"]) - float(end["charge_now"])
-    battery_life = float(duration) * (float(begin["charge_full"]) / battery_loss) 
-    print("\nTest complete")
-    print("Percent battery loss in",duration,"minutes is:",float(begin["percent"]) - float(end["percent"]),"%")
-    print("estimated battery life:",battery_life,"minutes -or-",battery_life/60)
-    
+ 
+    run_test(name,duration,apps,backlight)
